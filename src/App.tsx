3b1d4845b1db7e9f8578d1100bc4af079fef0606
adharainfinita <adharanosalevich@gmail.com>
2024-01-17 21:51:35 -0300

    first version finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MdSimCardDownload } from "react-icons/md";
+import { TiSocialInstagram, TiSocialPinterest } from "react-icons/ti";
+import { SiWattpad, SiSpotify } from "react-icons/si";
+import { text } from "./data/text";
+import "./App.css";
 
 function App() {
-  
-  return (
-    <>
-     
-    </>
-  )
+	return (
+		<>
+			<section>
+				<img
+					src="https://res.cloudinary.com/dlbhecdey/image/upload/v1705535265/nsiid4wlaeg5lrvitjgq.png"
+					alt="the blue book img"
+				/>
+				<img
+					src="https://res.cloudinary.com/dlbhecdey/image/upload/v1705528016/snxapndca8z1i04ugy13.png"
+					alt="the curse of Esker'lamet img"
+				/>
+			</section>
+			<section className="text">
+				{text.map((element) => {
+					return <div className="paragraph">{element}</div>;
+				})}
+			</section>
+			<section>
+				<div>
+					<p id="text-download">▪️ Descarga el libro aquí mismo 👇🥰</p>
+					<button>
+						<a href="La Maldicion de Esker'lamet.pdf" download={true}>
+							<MdSimCardDownload id="icon-download"/>
+						</a>
+					</button>
+				</div>
+			</section>
+      <section id="final-text">
+
+			<p > 👾👾👾 Y no te olvides de seguirme en mis redes sociales 👾👾👾</p>
+			<a href="https://www.instagram.com/sexta_vocal/" target="_blank">
+				<TiSocialInstagram className="social-icons" />
+			</a>
+			<a href="https://ar.pinterest.com/Adhara97/" target="_blank">
+				{" "}
+				<TiSocialPinterest className="social-icons" />
+			</a>
+			<a href="https://www.wattpad.com/user/Adh-astra" target="_blank">
+				{" "}
+				<SiWattpad className="social-icons" />
+			</a>
+			<a
+				href="https://open.spotify.com/user/217zlzbbvvkvpsjcyccaymv7a?si=97e7ddb3707c46d1"
+				target="_blank">
+          <SiSpotify className="social-icons" />
+        </a>
+          </section>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/data/text.ts
@@ -0,0 +1,10 @@
+const firstP: string =
+	"Hundida en la oscuridad de las calles frías de Jaiva, Artemisa sale de su casa una fría noche de otoño. Encaminada hacia el Hotel de Mario, la más lujosa red de prostitución del país, a cumplir con su trabajo, cuando un grupo de sicarios la intercepta para hacerle cobrar el dinero que le debe a la mafia paraguaya.";
+const secondP: string =
+	"Ella es una mujer trans, sobreviviente de la pandemia y de la sociedad. Ella paga lo que debe.";
+const thirdP: string =
+	"Al alba, Artemisa está muerta. Su cadáver, violentado y escarchado, es hallado por la policía.";
+const fourthP: string =
+	"Su muerte es tan solo el principio de una guerra por el poder que está a punto de estallar  . Y las consecuencias de ello involucrarán mucho más de lo que se espera. Dioses, demonios, sectas, y una maldición que une a tres mujeres atravesadas por el sufrimiento.";
+
+export const text: Array<string> = [firstP, secondP, thirdP, fourthP];
