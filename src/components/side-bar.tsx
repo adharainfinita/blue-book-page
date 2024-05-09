d6c3092c648075307029dca36599e323b0a9d082
Adhara <adharanosalevich@gmail.com>
2024-05-09 13:40:31 -0300

    enlaces a wtp

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -1,30 +1,69 @@
-import React from 'react';
-import '../styles/SideBar.css';
+import React from "react";
+import "../styles/SideBar.css";
 
+const Sidebar: React.FC = () => {
+	const expandSidebar = () => {
+		const sidebar = document.querySelector(".sidebar") as HTMLElement;
+		sidebar.classList.add("expanded");
+	};
 
-const Sidebar: React.FC= () => {
-  const expandSidebar = () => {
-    const sidebar = document.querySelector('.sidebar') as HTMLElement;
-    sidebar.classList.add('expanded');
-  };
+	const collapseSidebar = () => {
+		const sidebar = document.querySelector(".sidebar") as HTMLElement;
+		sidebar.classList.remove("expanded");
+	};
 
-  const collapseSidebar = () => {
-    const sidebar = document.querySelector('.sidebar') as HTMLElement;
-    sidebar.classList.remove('expanded');
-  };
-
-  return (
-    <aside className="sidebar" onMouseOver={expandSidebar} onMouseOut={collapseSidebar}>
-      <div className="sidebar-content">
-        <h4>Capítulo 1</h4>
-        <h4>Capítulo 2</h4>
-        <h4>Capítulo 3</h4>
-        <h4>Capítulo 4</h4>
-        <h4>Capítulo 5</h4>
-        <h4>Capítulo 6</h4>
-      </div>
-    </aside>
-  );
+	return (
+		<aside
+			className="sidebar"
+			onMouseOver={expandSidebar}
+			onMouseOut={collapseSidebar}
+		>
+			<div className="sidebar-content">
+				<a
+					href="https://www.wattpad.com/1328589875-el-libro-azul-la-maldici%C3%B3n-de-esker%27lamet-cap%C3%ADtulo"
+					target="_blank"
+				>
+					Capítulo 1
+				</a>
+				<a
+					href="https://www.wattpad.com/1288765845-el-libro-azul-la-maldici%C3%B3n-de-esker%27lamet-cap%C3%ADtulo"
+					target="_blank"
+				>
+					Capítulo 2
+				</a>
+				<a
+					href="https://www.wattpad.com/1294683051-el-libro-azul-la-maldici%C3%B3n-de-esker%27lamet-cap%C3%ADtulo"
+					target="_blank"
+				>
+					Capítulo 3
+				</a>
+				<a
+					href="https://www.wattpad.com/1309026663-el-libro-azul-la-maldici%C3%B3n-de-esker%27lamet-cap%C3%ADtulo"
+					target="_blank"
+				>
+					Capítulo 4
+				</a>
+				<a
+					href="https://www.wattpad.com/1310758912-el-libro-azul-la-maldici%C3%B3n-de-esker%27lamet-cap%C3%ADtulo"
+					target="_blank"
+				>
+					Capítulo 5
+				</a>
+				<a
+					href="https://www.wattpad.com/1311514051-el-libro-azul-la-maldici%C3%B3n-de-esker%27lamet-cap%C3%ADtulo"
+					target="_blank"
+				>
+					Capítulo 6: parte 1
+				</a>
+				<a
+					href="https://www.wattpad.com/1311516452-el-libro-azul-la-maldici%C3%B3n-de-esker%27lamet-cap%C3%ADtulo"
+					target="_blank"
+				>
+					Capítulo 6: parte 2
+				</a>
+			</div>
+		</aside>
+	);
 };
 
 export default Sidebar;
